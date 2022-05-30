5ab0076a61a3878205e456e4edd1a46cc6cb1e0f
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-29 20:04:35 -0700

    buttons

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ client.slashcommands = new Discord.Collection()
 client.button = new Discord.Collection()
 
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-client.loadSlashCommands = (bot, reload) => require("./handlers/button")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/buttons")(bot, reload)
 client.loadSlashCommands(bot, false)
-client.button(bot, false)
+client.loadbuttons(bot, false)
 
 client.on("interactionCreate", (interaction) => {
     if (!interaction.isCommand()) return 
