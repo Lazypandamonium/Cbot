28120454ab8dc3d8c5890236597e4665b44ff60d
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-29 20:48:31 -0700

    Update roleselection.js

--- commands/info/test/roleselection.js
+++ commands/info/test/roleselection.js
@@ -1,7 +1,7 @@
 const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
 
 module.exports = {
-    name: "roleselector",
+    name: "roleselection",
     category: "test",
     devOnly: true,
     run: async ({client, message, args}) => {
