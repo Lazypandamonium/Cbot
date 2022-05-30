01e1d7d47818a75ed3266abafc1ca13d0613f11b
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-29 17:41:01 -0700

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
-
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES",
-        "GUILD_MEMBERS"
     ]
 })
 
