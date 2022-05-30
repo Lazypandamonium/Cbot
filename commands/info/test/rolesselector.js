9bff316894372cf75cc31f3f19825ac81ae2bac5
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-29 22:01:06 -0700

    Update rolesselector.js

--- commands/info/test/rolesselector.js
+++ commands/info/test/rolesselector.js
@@ -7,11 +7,11 @@ module.exports = {
     run: async ({client, message, args}) => {
         message.channel.send({
             embeds: [
-                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("BLUE")
+                new MessageEmbed().setTitle("").setDescription("Select roles from the buttons below").setColor("BLUE")
             ],
             components: [
                 new MessageActionRow().addComponents([
-                    new MessageButton().setCustomId("role-980676343752695849").setStyle("PRIMARY").setLabel("5")
+                    new MessageButton().setCustomId("role-980676343752695849").setStyle("PRIMARY").setLabel("Hi")
                 ])
             ]
         })
