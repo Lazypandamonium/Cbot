aa9f664820c3f5d73820854f14377b586b19b157
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-30 09:03:54 -0700

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -4,19 +4,19 @@ const { getFiles } = require("../util/functions")
 module.exports = (bot, reload) => {
     const {client} = bot
 
-    let events =getFiles("./events/", ".js")
+    let events = getFiles("./events/", ".js")
 
     if (events.length === 0){
         console.log("No events to load")
     }
 
     events.forEach((f, i) => {
-        if (reload)
+        if (reload) 
             delete require.cache[require.resolve(`../events/${f}`)]
         const event = require(`../events/${f}`)
         client.events.set(event.name, event)
 
-        if (reload)
+        if (!reload)
             console.log(`${i + 1}. ${f} loaded`)
     })
 
@@ -25,13 +25,13 @@ module.exports = (bot, reload) => {
 }
 
 function triggerEventHandler(bot, event, ...args){
-    const {client} = bot
+    const {client} = bot 
 
     try {
         if (client.events.has(event))
             client.events.get(event).run(bot, ...args)
-        else
-            throw new Error(`Event ${event} doesnot exist`)
+        else 
+            throw new Error(`Event ${event} does not exist`)
     }
     catch(err){
         console.error(err)
@@ -39,14 +39,14 @@ function triggerEventHandler(bot, event, ...args){
 }
 
 function initEvents(bot) {
-    const {client} = bot
+    const {client} = bot 
 
     client.on("ready", () => {
         triggerEventHandler(bot, "ready")
     })
 
     client.on("messageCreate", (message) => {
-       triggerEventHandler(bot, "messageCreate", message) 
+        triggerEventHandler(bot, "messageCreate", message)
     })
 
     client.on("interactionCreate", (interaction) => {
