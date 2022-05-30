b63e20241b9e37a408d57bfe584da4c32e72b5ef
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-30 10:16:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-// const generateImage = require("./generateImage")
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
@@ -34,23 +34,23 @@ client.loadSlashCommands(bot, false)
 
 module.exports = bot
 
-// client.on("ready", () => {
-//     console.log(`Logged in as ${client.user.tag}`)
-// })
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
-// client.on("messageCreate", (message) =>{
-//     if (message.content == "hi")
-//     message.reply("Welcome enjoy your stay!")
-// })
+client.on("messageCreate", (message) =>{
+    if (message.content == "hi")
+    message.reply("Welcome enjoy your stay!")
+})
 
-// const welcomeChannelId = "978688803663380512"
+const welcomeChannelId = "978688803663380512"
 
-// client.on("guildMemberAdd", async (member) => {
-//     const img = await generateImage(member)
-//     member.guild.channels.cache.get(welcomeChannelId).send({
-//         content: `<@${member.id}> Welcome to the server!`,
-//         files: [img]
-//     })
-// })
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
 
 client.login(process.env.TOKEN)
