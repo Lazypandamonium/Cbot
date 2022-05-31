18ee02a74e5afaa3d8df5666ced36a03c5307b2f
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-30 22:10:55 -0700

    Update hobbies.js

--- commands/test/hobbies.js
+++ commands/test/hobbies.js
@@ -7,7 +7,7 @@ module.exports = {
     run: async ({client, message, args}) => {
         message.channel.send({
             embeds: [
-                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("BLUE")
+                new MessageEmbed().setTitle("Select Hobbie").setDescription("Select Hobbie from the buttons below").setColor("BLUE")
             ],
             components: [
                 new MessageActionRow().addComponents([
