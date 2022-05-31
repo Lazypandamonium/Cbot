cca6c58e15e131f895cd421e38f7d34bae0abff6
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-30 21:15:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new Discord.Client({
 let bot = {
     client,
     prefix: "n.",
-    owners: ["978688803663380510"]
+    owners: ["369555026219106304"]
 }
 
 client.commands = new Discord.Collection()
