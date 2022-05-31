9322821c86a3bd1fb821ca7a21d770b459a89b38
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-30 21:41:34 -0700

    Update rolesselector.js

--- commands/test/rolesselector.js
+++ commands/test/rolesselector.js
@@ -14,8 +14,7 @@ module.exports = {
                     new MessageButton().setCustomId("role-980526645272272986").setStyle("PRIMARY").setLabel("Black"),
                     new MessageButton().setCustomId("role-980526606080704542").setStyle("PRIMARY").setLabel("White"),
                     new MessageButton().setCustomId("role-980526551156285520").setStyle("PRIMARY").setLabel("Pink"),
-                    new MessageButton().setCustomId("role-980526526888038410").setStyle("PRIMARY").setLabel("Purple"),
-                    new MessageButton().setCustomId("role-980526476099223552").setStyle("PRIMARY").setLabel("Green"),                   
+                    new MessageButton().setCustomId("role-980526526888038410").setStyle("PRIMARY").setLabel("Purple"),                    
                 ])
             ]
         })
