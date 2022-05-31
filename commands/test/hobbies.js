b0fbe7672d5b06a72857495ed7f050edb86ac1db
Lazypandamonium <eight.grades_0u@icloud.com>
2022-05-30 22:05:09 -0700

    Create hobbies.js

---
+++ commands/test/hobbies.js
@@ -0,0 +1,23 @@
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "hobbies",
+    category: "test",
+    devOnly: true,
+    run: async ({client, message, args}) => {
+        message.channel.send({
+            embeds: [
+                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("BLUE")
+            ],
+            components: [
+                new MessageActionRow().addComponents([
+                    new MessageButton().setCustomId("role-980529645864775750").setStyle("PRIMARY").setLabel("Athele"),
+                    new MessageButton().setCustomId("role-980529750470721596").setStyle("PRIMARY").setLabel("Gym Rat"),
+                    new MessageButton().setCustomId("role-980529587421323294").setStyle("PRIMARY").setLabel("Reader"),
+                    new MessageButton().setCustomId("role-980529485625585705").setStyle("PRIMARY").setLabel("Gamer"),
+                    new MessageButton().setCustomId("role-980529708708028561").setStyle("PRIMARY").setLabel("Degnerate"),
+                ])
+            ]
+        })
+    }
+}
