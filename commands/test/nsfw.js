458a44c4b98f2fb2b6cf5878a77129943d75785f
Lazypandamonium <eight.grades_0u@icloud.com>
2022-06-01 10:30:01 -0700

    Create nsfw.js

---
+++ commands/test/nsfw.js
@@ -0,0 +1,19 @@
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "hobbies",
+    category: "test",
+    devOnly: true,
+    run: async ({client, message, args}) => {
+        message.channel.send({
+            embeds: [
+                new MessageEmbed().setTitle("AT YOUR OWN RISK").setDescription("NSFW").setColor("BLUE")
+            ],
+            components: [
+                new MessageActionRow().addComponents([
+                    new MessageButton().setCustomId("role-981610281593622579").setStyle("PRIMARY").setLabel("NSFW")
+                ])
+            ]
+        })
+    }
+}
